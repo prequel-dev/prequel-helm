cd0f5ba735aee3214e628e1637ba5ea828084a9a
Tony Meehan <tonymeehan@users.noreply.github.com>
2025-08-13 11:49:12 -0500

    Update CI

--- ci/patch.sh
+++ ci/patch.sh
@@ -3,12 +3,12 @@
 set -x
 
 if [ "$#" -eq 1 ]; then
-  echo "Usage: $0 <path-to-helm-values> <path-to-release-output-yaml>"
+  echo "Usage: $0 <path-to-helm-values> <path-to-version-file>"
   exit 1
 fi
 
 VALUES_YAML=$1
-REL_OUT=$2
+VERSION_FILE=$2
 
 function do_client() {
   CLIENT_IMAGES=(
@@ -21,22 +21,13 @@ function do_client() {
 
     IFS=":" read -r name repo <<< "$image"
 
-    echo Current $name hash:
+    echo Current $name tag:
     cat $VALUES_YAML | yq ".$name.image.tag"
 
-    HASH=$(cat $REL_OUT | yq ".publishedReleaseRepos[] | select(.name==\"$repo\").hash")
+    echo New $name tag:
+    echo $(cat $VERSION_FILE)
 
-    echo New $name hash:
-    echo $HASH
-
-     if [[ -z $HASH ]]; then
-      echo "Skipping $repo due to empty hash"
-    else
-      yq -i ".$name.image.tag = \"$HASH\"" $VALUES_YAML
-      yq -i ".$name.image.repoName = \"$repo\"" $VALUES_YAML  
-    fi
-
-    echo $?
+    yq -i ".$name.image.tag = \"$(cat $VERSION_FILE)\"" $VALUES_YAML
 
   done
 }
